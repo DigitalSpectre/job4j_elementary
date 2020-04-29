e519de1f5d9c1871060d73968d75d0b8d7822238
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2020-04-29 14:23:33 +0300

    6.0.2. Заполнение массива.[#247297]

--- src/ru/job4j/attay/ArrayDefinition.java
+++ src/ru/job4j/attay/ArrayDefinition.java
@@ -5,5 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Katya";
+        names[1] = "Tanya";
+        names[2] = "Olya";
+        names[3] = "Yulya";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
