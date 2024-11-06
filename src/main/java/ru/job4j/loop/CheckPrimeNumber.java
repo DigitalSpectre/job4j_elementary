b0c6103c35d04b6ad39c124cc6e8cbf9522a943d
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-11-06 19:42:33 +0300

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean isPrime = true;
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+}
