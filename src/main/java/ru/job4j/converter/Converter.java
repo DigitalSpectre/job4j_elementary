cfa85ec3c393d376da30e08ea7140f3de6f8e1b7
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-11-06 08:24:50 +0300

    1. Модульные тесты [#505277 #517072]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -5,7 +5,6 @@ public class Converter {
         // Assuming the conversion rate is 1 Euro = 90 Rubles
         return value / 90;
     }
-
     public static double rubleToDollar(double value) {
         // Assuming the conversion rate is 1 Dollar = 75 Rubles
         return value / 75;
