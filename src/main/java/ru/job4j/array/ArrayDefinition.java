301cc8704f4a2aa9b7e7b1a208b59286a5b23f6a
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-11-10 03:49:45 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Jane";
+        names[2] = "Doe";
+        names[3] = "Bob";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
         System.out.println("ages: " + ages.length);
         System.out.println("surnames: " + surnames.length);
